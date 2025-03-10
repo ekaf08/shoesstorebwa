2f0a34b9e2bb0db0f809ca97d0c31619abf6a8d6
ekaf08 <105408310+ekaf08@users.noreply.github.com>
2025-03-10 14:54:08 +0700

    relasi categori to shoes

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -3,8 +3,24 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Category extends Model
 {
-    //
+    use HasFactory, SoftDeletes;
+
+    protected $guarded = ['id'];
+    protected $table = 'categories';
+
+    public function shoes(): HasMany
+    {
+        return $this->hasMany(Shoes::class);
+    }
+
+    public function setNameAttribute($value)
+    {
+        $this->attributes['name'] = $value;
+        $this->attributes['slug'] = Str::slug($value);
+    }
 }
--- app/Models/Shoes.php
+++ app/Models/Shoes.php
@@ -3,8 +3,28 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Shoes extends Model
 {
-    //
+    use HasFactory, SoftDeletes;
+
+    protected $guarded = ['id'];
+
+    public function setNameAttribute($value)
+    {
+        $this->attributes['name'] = $value;
+        $this->attributes['slug'] = Str::slug($value);
+    }
+
+    public function brand(): BelongsTo
+    {
+        return $this->belongsTo(Brand::class, 'brand_id');
+    }
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class, 'category id');
+    }
 }
